da9aa63d4d058a55027d51e205cf494e49c2da86
Adam jalil <johndoe@example.com>
2024-04-15 13:50:41 +0200

    requirement

--- visu.py
+++
@@ -1,8 +0,0 @@
-import plotly.express as px
-import pandas as pandas
-
-country_df = pandas.read_csv('datasets/Country-data.csv')
-fig = px.choropleth (country_df, locationmode = 'country names', locations = 'country', color = 'inflation',
-                     title = "Distribution of 'GDPP' in different countries of the world"
-                     , color_continuous_scale = "piyg")
-fig.show()
